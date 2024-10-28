feb41284cfcfae01640e866901b94f090f130ebc
Anton Shapovalov <anton.shapovalov.5@gmail.com>
2024-10-28 13:08:33 +0000

    Initial commit

---
+++ src/main/kotlin/concept/stc/SpringThymeleafCoroutinesApplication.kt
@@ -0,0 +1,11 @@
+package concept.stc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringThymeleafCoroutinesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringThymeleafCoroutinesApplication>(*args)
+}
---
+++ src/test/kotlin/concept/stc/SpringThymeleafCoroutinesApplicationTests.kt
@@ -0,0 +1,13 @@
+package concept.stc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringThymeleafCoroutinesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
