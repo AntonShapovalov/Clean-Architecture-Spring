49aa6c6a87d55cb9528350777ab5b31ebe590c30
Anton Shapovalov <anton.shapovalov.5@gmail.com>
2024-11-25 20:20:17 +0000

    Set default config value

--- src/test/kotlin/concept/stc/config/OmdbApiConfigIntegrationTest.kt
+++ src/test/kotlin/concept/stc/config/OmdbApiConfigIntegrationTest.kt
@@ -1,23 +1,20 @@
 package concept.stc.config
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
-@AutoConfigureTestDatabase(replace = Replace.ANY)
 class OmdbApiConfigIntegrationTest {
 
     @Autowired
-    private lateinit var configProperties: OmdbApiConfigProperties
+    private lateinit var properties: OmdbApiConfigProperties
 
     @Test
     fun `when getting properties, given config, then values are not empty`() {
-        assertTrue(configProperties.baseUrl.isNotEmpty())
+        assertTrue(properties.baseUrl.isNotEmpty())
         // Be sure that "secrets.properties" contains the api key
         // assertTrue(configProperties.apiKey.isNotEmpty())
     }
