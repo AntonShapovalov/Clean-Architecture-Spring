932deb6b5b31283bb38387cb99af077a08fa0589
Anton Shapovalov <anton.shapovalov.5@gmail.com>
2024-12-25 20:59:55 +0000

    Correct documentation for ApiService

--- src/main/kotlin/concept/stc/data/ApiService.kt
+++ src/main/kotlin/concept/stc/data/ApiService.kt
@@ -11,9 +11,11 @@ import org.springframework.stereotype.Service
 
 /**
  * Data service to manage API operations.
+ * In opposite to the domain service, that operates with pure domain models,
+ * this service operates with "raw" data, as API response and database entities.
  *
  * @param apiClient the API client.
- * @param repository the movie repository.
+ * @param repository the CRUD movie repository.
  */
 @Service
 class ApiService(
@@ -25,6 +27,8 @@ class ApiService(
      * Load movies from the external API and save them to the database.
      *
      * @param title the movie title.
+     *
+     * @return the movie flow that emits the saved movies.
      */
     suspend fun loadMovies(title: String): Flow<Movie> {
         val movies = apiClient.search(title).movies
